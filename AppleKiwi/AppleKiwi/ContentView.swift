f18d1174faaf8d4c0fc86a770647200db7c30f17
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2022-04-06 17:11:30 +0900

    마이페이지 틀

--- AppleKiwi/AppleKiwi/ContentView.swift
+++ AppleKiwi/AppleKiwi/ContentView.swift
@@ -1,4 +1,4 @@
-//
+    //
 //  ContentView.swift
 //  AppleKiwi
 //
@@ -9,11 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
+        VStack() {
+            HStack {
+                Text("마이페이지")
+                    .padding().font(.title)
+                Spacer()
+            }.background(.white)
+            
+            HStack{ Image("test")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+                    .aspectRatio(contentMode: .fit)
+                            .frame(width: 100)
+                            .clipShape(Circle()).padding()
+                
+                Text("changBro chaleea22@pos.idserve.net")
+                    .font(.body)
+                    .frame(minWidth: 0, idealWidth: 100, maxWidth: 400, minHeight: 0, maxHeight: 100, alignment: .center)
 
+                
+                Spacer()
+            }
+            
+            List {
+                Button("알림설정") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                Button("비밀번호 변경") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                Button("기본 방침") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                Button("회원 탈퇴") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+             
+            }
+                
+            }
+        }
+    }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
